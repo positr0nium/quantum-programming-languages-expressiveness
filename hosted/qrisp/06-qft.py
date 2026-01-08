7d7b8c47d581e02e5dfdc95355bb09f0b786e7c5
positr0nium <raphaelsdel@gmail.com>
2026-01-08 10:19:33 +0100

    updated updated qft implementation

--- hosted/qrisp/06-qft.py
+++ hosted/qrisp/06-qft.py
@@ -1,19 +1,6 @@
 # Quantum Fourier Transform (QFT) in Qrisp
 import qrisp as q
-import numpy as np
-
-def qft_qrisp(n):
-    qv = q.QuantumVariable(n)    
-    # Apply QFT rotations
-    for i in range(n):
-        q.h(qv[i])
-        for j in range(i+1, n):
-            q.cp(np.pi / 2 ** (j-i), qv[j], qv[i])
-    
-    # Apply swaps to correct order
-    for i in range(n//2):
-        q.swap(qv[i], qv[n-i-1])    
-    return qv.qs.compile()
-
 n = 6
-print(qft_qrisp(n))
+qv = q.QuantumVariable(n)
+q.QFT(qv)
+print(qv.qs)
